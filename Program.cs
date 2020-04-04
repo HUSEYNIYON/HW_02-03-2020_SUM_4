aa9daa5c0ff15c5d311ea53a8ee51ace7fb6b2d5
HUSEYNIYON <your>
2020-04-04 17:08:56 +0300

    created console.net core project

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ class Car
     {
         public string carName;
         public int carSpeed;
+         public void SayAbout()
+        {
+            Console.WriteLine($"{carName} is going {carSpeed} km/h");
+        }
+        public void speedIncrease(int increase)
+        {
+            carSpeed=carSpeed+increase;
+        }
     }
     class Program
     {
